f44764ca5e7d5f8dd284d53a597bad19eb761164
Jon Distad <jon.distad@gmail.com>
2012-02-12 03:09:12 -0500

    debug output

--- include/uberlisp/gc.h
+++ include/uberlisp/gc.h
@@ -1,6 +1,8 @@
 #ifndef _UBERLISP_GC_H
 #define _UBERLISP_GC_H
 
+#include <stdio.h>
+#include <avr/pgmspace.h>
 #include <uberlisp/types.h>
 
 void __GC__();
--- src/gc.c
+++ src/gc.c
@@ -1,17 +1,22 @@
 #include <uberlisp/gc.h>
 
 void __GC__() {
+  printf_P(PSTR("Beginning GC...\n"));
   int garbage;
   do {
+    printf_P(PSTR("Top of garbage loop.\n"));
     garbage = 0;
     uptr_t *cur = UPTR_PTR(CSTART_p);
+    printf_P(PSTR("Marking..."));
     while (cur < PTREND_p) {
       if (IS_CONS(*cur)) *UPTR_PTR(*cur) |= GC_FLAG;
       cur++;
     }
+    printf_P(PSTR("Done!\n"));
 
     uptr_t *free_st, *free_end;
     cur = UPTR_PTR(CSTART_p);
+    printf_P(PSTR("Sweeping...\n"));
     while (cur < UPTR_PTR(CEND_p)) {
       if (! (*cur & GC_FLAG)) {
         garbage = 1;
@@ -22,16 +27,22 @@ void __GC__() {
         ++cur;
         free_end = cur;
         int free_bytes = (intptr_t)free_end - (intptr_t)free_st;
+        printf_P(PSTR("Freeing %i bytes...\n"), free_bytes);
         memmove(CPTR(CSTART_p + free_bytes), CPTR(CSTART_p), free_bytes);
         memset(CPTR(CSTART_p), 0, free_bytes);
         CSTART_p += free_bytes;
         for (; cur < PTREND_p; ++cur)
           if (IS_CONS(*cur) && TO_PTR(*cur) < CSTART_p) *cur += free_bytes;
         cur = free_end;
-      }
+      } else
+        ++cur;
     }
-
+    printf_P(PSTR("Done!\n"));
+    
+    printf_P(PSTR("Clearing GC bits..."));
     for (cur = UPTR_PTR(CSTART_p); cur < UPTR_PTR(CEND_p); ++cur)
       *cur &= ~GC_FLAG;
+    printf_P(PSTR("Done!\n"));
   } while (garbage);
+  printf_P(PSTR("GC Complete!\n"));
 }
